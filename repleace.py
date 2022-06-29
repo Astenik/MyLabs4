813e2de7158ed4fedb4d14a04292ab11038f7449
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-29 22:41:26 +0400

    Update repleace.py

--- repleace.py
+++ repleace.py
@@ -1,34 +1,30 @@
-def repleace(source, old, new):
+def  repleace(source, old, new, count = -1):
         '''this funtion repleace old with new in source.'''
-        i = 0
-        while len(source) - i >= len(old):
-             string = ''
-             lst = []
-             for j in range(i, len(source)): 
-                    string += source[j]
-                    if len(string) == len(old):
-                         lst.append(string)
-                         string = ''
-                         continue
-                    else:
-                        continue
-             for ind in range(len(lst)):
-                  if lst[ind] == old:
-                       name = ''
-                       for ind1 in range(i):
-                             name += source[ind1]
-                       for ind2 in range(ind):
-                             name += lst[ind2]
-                       name += new
-                       for ind3 in range(i + (ind + 1)*len(old), len(source)):
-                             name += source[ind3]
-                       source = name
+       index = 0
+       name = source
+       num = old[0]
+       q = 0
+       while  name != None:
+            for i in range(len(source)):
+                 if (source[i] == num) and (source[i:(i + len(old))] == old):
+                       index = i
+                       q += 1
+                       break
+                 elif source[i] != num:
                        continue
-                  else:
-                       continue
-             i += 1
-        return name
-name = input("insert name: ")
-old = input("insert the old name: ")
-new = input("insert new name: ")
-print(repleace(name, old, new))
+            string = ''
+            string += source[:index]
+            string += new
+            string += source[(index + len(old)):]
+            source = string
+            if q == count:
+                  break
+            else:
+                 continue
+            name = source[(index + len(old)):]
+       return string
+source = input("insert your string: ")
+old = input("insert old string: ")
+new = input("insert new string: ")
+count = int(input("insert count: "))
+print(f'your new string is: {repleace(source, old, new, count)}')
