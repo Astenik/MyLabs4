f56e05c7a171bcb0a7a772307d6612aab5853a28
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-30 17:07:54 +0400

    Update split.py

--- split.py
+++ split.py
@@ -1,35 +1,31 @@
-def  split(source, sep, count = -1):
-       '''this function returns the list consists of your source string that is splited with sep string.
+def split(source, sep, count = -1):
+        '''this function returns the list consists of your source string that is splited with sep string.
        source is the first argument of function. sep is the secont argument of function.
        the third argument is count that defoult is -1.'''
        lst = []
-       index = 0
-       j = 0
-       num = sep[0]
+       ind = 0
        q = 0
-       while  j < len(source):
-            name = ''
-            for i in range(len(source)):
-                 if (source[i] == num) and (source[i:(i + len(sep))] == sep):
-                       index = i
-                       q += 1
-                       break
-                 elif source[i] != num:
-                       name += source[i] 
-                       continue
-            lst.append(name)
-            string = ''
-            for ind in range(index + len(sep), len(source)):
-                  string += source[ind]
-            source = string
-            if q == count:
-                  lst.append(source)
-                  break
-            j += 1
+       i = 0
+       while i < len(source):
+             if source[i:(i + len(sep))] == sep:
+                   lst.append(source[ind:i])
+                   ind = i + len(sep)
+                   q += 1
+                   i += len(sep)
+                   if q == count:
+                         lst.append(source[ind:])
+                         break
+             else:
+                    i += 1
+             if len(source[i:]) < len(sep):
+                    lst.append(source[ind:])
+                    break
+             else:
+                   continue
        return lst
+
 
 string = input("insert your string: ")
 sep = input("insert the sep: ")
 count = int(input("insert the count: "))
 print(f"your list consists of your string is: {split(string, sep, count)}")
-         
