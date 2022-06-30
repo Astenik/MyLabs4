c3a106360ed8eeb0d30f56f47c3dfdf8f9fb98dd
Astenik <106985461+Astenik@users.noreply.github.com>
2022-06-30 16:27:34 +0400

    Update repleace.py

--- repleace.py
+++ repleace.py
@@ -1,20 +1,20 @@
 def repleace(source, old, new, count = -1):
        q = 0
        for i in range(len(source)):
-              if source[i:(i + len(old)] == old:
+               if source[i:(i + len(old))] == old:
                        string = ''
                        string += source[:i]
                        string += new
-                       string += source[(i + len(old):]
+                       string += source[(i + len(old)):]
                        source = string
                        q += 1
-              if q == count:
+               if q == count:
                         break
-              else:
+               else:
                         continue
        return string
 
 source = input("insert the string: ")
 old = input("insert old string: ")
 new = input("insert new string: ")
-print(f'your new string is: {repleace(source, old, new)}')
+print(f'your new string is: {repleace(source, old, new, 2)}')
